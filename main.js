0f178d764993ba6f8b159dfa5101d8bd52b827d8
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2023-11-16 11:10:45 +0100

    chaned player border color

--- main.js
+++ main.js
@@ -77,17 +77,6 @@ function play_round() {
     choice_button.classList.remove("winner");
   }
 
-  sleep(2000);
   play_button.disabled = false;
-  cpu_choice.classList.remove("winner");
-  choice_button.classList.remove("winner");
-}
 
-function sleep(ms) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > ms){
-      break;
-    }
-  }
 }
